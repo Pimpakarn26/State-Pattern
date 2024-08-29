b6023689ca0ccdbbc4e23fb28d5a69eff42c69c7
Pimpakarn26 <bonemarrow26bp.gmail.com>
2024-08-29 16:38:23 +0700

    first commit

---
+++ src/GumballMachineTestDrive.java
@@ -0,0 +1,11 @@
+
+public class GumballMachineTestDrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GumballMachine gumballMachine = new GumballMachine(3);
+		gumballMachine.turnCrank();
+	}
+}
+
+//thinking count = , state
